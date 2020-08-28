0f7e7a59ca5843172f89ad89ec4838bfc0ae84a0
ctartt-c <carlton.tartt@bryanuniversity.edu>
2020-08-28 06:20:17 -0500

    strings

---
+++ week4/string_method/string.js
@@ -0,0 +1,23 @@
+//lesson1
+
+var name = "myCar";
+
+var upperCasedMyCar = name.toUpperCase()
+console.log(upperCasedMyCar)
+
+
+var lowerCasedMyCar = name.toLowerCase()
+console.log(lowerCasedMyCar)
+
+//lesson2
+
+function halfNum(num) {
+  return Math.floor(num / 2)
+}
+var stringResult = halfNum(23);
+console.log(stringResult);
+
+//lesson3
+var firstName = "carltonTartt" 
+var last3 = firstName.slice(10)
+console.log(last3)
